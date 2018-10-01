d94cb3832ccdff056268e94aa789421a6e5e3ee6
mahi727 <mahibui9@gmail.com>
2018-10-02 00:49:08 +0600

    first

---
+++ application/views/welcome.php
@@ -0,0 +1,185 @@
+<style>
+    background-attachment: scroll;
+    background-clip: border-box;
+    background-color: rgb(255, 255, 255);
+    background-image: none;
+    background-origin: padding-box;
+    background-position: 0% 0%;
+    background-position-x: 0%;
+    background-position-y: 0%;
+    background-repeat: repeat;
+    background-size: auto auto;
+
+    style.css:77
+    header
+    auto auto
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+
+    style.css:13
+    *
+    0px
+     border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    color: rgb(255, 255, 255);
+    display: block;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 13px;
+    line-height: 20.8px;
+    position: fixed;
+    width: 1287px;
+    z-index: 999;
+</style>
+
+<!--=== Top ===-->
+<div class="top">
+    <div class="container">
+        <ul class="loginbar pull-right">
+
+            <li><a href="http://www.iit.du.ac.bd/users/login" class="login-btn">Login</a></li>
+            <li class="devider">&nbsp;</li>
+            <li><a href="http://www.iit.du.ac.bd/about_iit/faq" class="login-btn">Help</a></li>
+        </ul>
+    </div>
+</div>
+<!--/top-->
+<!--=== End Top ===-->
+
+<!--=== Header ===-->
+<div class="header">
+    <div class="container">
+        <!-- IIT Logo -->
+        <div class="logo">
+            <img id="iitlogo-header" src="http://www.iit.du.ac.bd/assets/img/iitlogo-blue.png" style="width:8em;" alt="IIT Logo">
+        </div>
+        <!-- /logo -->
+
+        <!-- Menu -->
+        <div class="navbar">
+
+            <div class="navbar-inner">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a><!-- /nav-collapse -->
+                <div class="nav-collapse collapse">
+                    <ul class="nav top-2" style="text-align:left;">
+                        <li class="active">
+                            <a href="http://www.iit.du.ac.bd/home">Home</a>
+                        </li>
+                        <li>
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown">About IIT
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="http://www.iit.du.ac.bd/about_iit/institution">Institution</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/about_iit/aims_and_objectives">Aims and Objectives</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/about_iit/history">History</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/about_iit/faculty">Faculty</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/about_iit/staff">Officers and Staffs</a></li>
+                            </ul>
+                            <b class="caret-out"></b>
+                        </li>
+                        <li>
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown">Academic
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="http://www.iit.du.ac.bd/academic/undergraduate_first">Undergraduate Studies</a>
+                                </li>
+                                <li><a href="http://www.iit.du.ac.bd/academic/graduate_first">Graduate Studies</a>
+                                </li>
+                                <li><a href="http://www.iit.du.ac.bd/academic/training_programs_first">Training Programs</a>
+                                </li>
+                            </ul>
+                            <b class="caret-out"></b>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Notices
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="http://www.iit.du.ac.bd/notice/general_first">General</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/notice/undergraduate_first">Undergraduate Studies</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/notice/graduate_first">Graduate Studies</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/notice/training_programs_first">Training Programs</a></li>
+                            </ul>
+                            <b class="caret-out"></b>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Life in IIT
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="http://www.iit.du.ac.bd/view_news_events">News &amp; Events</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/upcoming_events">Upcoming Events</a></li>
+                                <li><a href="http://www.iit.du.ac.bd/view_achievements">Achievements</a></li>
+                            </ul>
+                            <b class="caret-out"></b>
+                        </li>
+                        <li>
+                            <a href="http://www.iit.du.ac.bd/research_areas">Research</a>
+                        </li>
+                        <li><a href="http://www.iit.du.ac.bd/contact/contact_page">Contact</a></li>
+                        <li class="open-menu dropdown"><a href="#browse-menu" class="dropdown-toggle" data-toggle="dropdown">Site Map <b class="caret"></b></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /nav-collapse -->
+            </div>
+            <!-- /navbar-inner -->
+        </div>
+        <!-- /navbar -->
+        <div class="du-logo">
+            <img class="dulogo-header" src="http://www.iit.du.ac.bd/assets/img/dulogo.jpg" alt="DU Logo">
+        </div>
+    </div>
+    <!-- /container -->
+</div>
+<!--/header -->
+<!--=== End Header ===-->
+<section id="browse-menu" style="display: none;">
+    <div class="container">
+        <div class="row">
+            <div class="span2"></div>
+            <div class="span8">
+                <ul>
+                    <li><span class="head-menu">About IIT</span>
+                        <ul>
+                            <li><a href="http://www.iit.du.ac.bd/about_iit/institution">Institution</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/about_iit/aims_and_objectives">Aims and Objectives</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/about_iit/history">History</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/about_iit/faculty">Faculty</a></li>
+                        </ul>
+                    </li>
+                    <li><span class="head-menu">Academic</span>
+                        <ul>
+                            <li><a href="http://www.iit.du.ac.bd/academic/undergraduate_first">Undergraduate Studies</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/academic/graduate_first">Graduate Studies</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/academic/training_programs_first">Training Programs</a></li>
+                        </ul>
+                    </li>
+                    <li><span class="head-menu">Notices</span>
+                        <ul>
+                            <li><a href="http://www.iit.du.ac.bd/notice/general_first">General</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/notice/undergraduate_first">Undergraduate Studies</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/notice/graduate_first">Graduate Studies</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/notice/training_programs_first">Training Programs</a></li>
+                        </ul>
+                    </li>
+                    <li><span class="head-menu">Life in IIT</span>
+                        <ul>
+                            <li><a href="http://www.iit.du.ac.bd/view_news_events">News &amp; Events</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/upcoming_events">Upcoming Events</a></li>
+                            <li><a href="http://www.iit.du.ac.bd/view_achievements">Achievements</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
